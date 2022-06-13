b9c53645880bb5afc7b9b59a331f017f93899ff0
Ashish K <ashishskabade@gmail.com>
2022-06-13 15:00:46 +0530

    clean the code

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
 const http= require('http');
 const fs=require('fs');
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url==='/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body id="body"><form action="/message" method="POST" ><input id="input" type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    //function createli(){}
-    if(url==='/message' && method==="POST"){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parseBody=Buffer.concat(body).toString();
-            var message=parseBody.split("=")[1];
-            fs.writeFileSync('message.txt',message);
-        });
-        res.statusCode=302;
-        res.setHeader('location','/');
-        return res.end();
-    }
-        res.setHeader('contentType','text/html');
-        res.write('<html>');
-        res.write('<head><title>My First WebPage</title></head>');
-        res.write('<body><h1>My First WebPage</h1></body>');
-        res.write('</html>');
-        res.end();
-    
-});
+const requestHandler=require('./routes');
+const server=http.createServer(requestHandler);
 server.listen(4000);
