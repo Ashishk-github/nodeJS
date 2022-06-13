19350b6ba40a7499d065e36710b2f3df6914e6d8
Ashish K <ashishskabade@gmail.com>
2022-06-13 11:14:08 +0530

    webpage on localhost

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const http= require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log('Ashish')
-    process.exit();
+    const url=req.url;
+    if(url==='/home'){
+        res.write('<html>');
+        res.write('<head><title>Home Page</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(url==='/about'){
+        res.write('<html>');
+        res.write('<head><title>About Page</title></head>');
+        res.write('<body><h1>Welcome to About us Page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(url==='/node'){
+        res.write('<html>');
+        res.write('<head><title>NodeJS Page</title></head>');
+        res.write('<body><h1>Welcome to my NodeJS Page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else{
+        res.setHeader('contentType','text/html');
+        res.write('<html>');
+        res.write('<head><title>My First WebPage</title></head>');
+        res.write('<body><h1>My First WebPage</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 server.listen(4000);
