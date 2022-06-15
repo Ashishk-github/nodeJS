7bee22b952463577f147267e18080587da0af2bf
Ashish K <ashishskabade@gmail.com>
2022-06-15 16:16:47 +0530

    code cleanup and flter root

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const bodyParser=require('body-parser');
 
 const app=express();
 
+const adminRouter=require('./routes/admin');
+const shopRouter=require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="qty"><button type="submit">ADD PRODUCT</button></form>');
-});
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello People</h1>');
-});
-
+app.use(bodyParser.json());
+app.use('/admin',adminRouter);
+app.use('/shop',shopRouter)
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>PAGE NOT FOUND</h1>')
+})
 app.listen(4000);
