95621403b8c317be435c4cdb7d00d4eb3774a121
Ashish K <ashishskabade@gmail.com>
2022-06-15 13:31:38 +0530

    express

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const http= require('http');
-const fs=require('fs');
-const requestHandler=require('./routes.js');
-const server=http.createServer(requestHandler.handler);
-server.listen(4000);
+//const http= require('http');
+//const fs=require('fs');
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('In the next middleware');
+    res.send('<h1>Hello People</h1>');
+});
+
+//const server=http.createServer(app);
+app.listen(4000);
