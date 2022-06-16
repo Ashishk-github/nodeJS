2b8bceca6ec97f0c565ed8e4f69d70eec8bed849
Ashish K <ashishskabade@gmail.com>
2022-06-16 14:37:40 +0530

    style and html

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-
+const path=require('path');
+const rootDir=require('./util/path');
 const app=express();
 
 const adminRouter=require('./routes/admin');
 const shopRouter=require('./routes/shop');
+const contactUs= require('./routes/contact');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRouter);
-app.use('/shop',shopRouter)
+app.use('/',contactUs);
+app.use('/',shopRouter);
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>PAGE NOT FOUND</h1>')
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'));
 })
-app.listen(4000);
+app.listen(3000);
